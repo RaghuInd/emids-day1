194406c304097a3899e0824183f916caccec9f9e
RaghuInd <117819856+RaghuInd@users.noreply.github.com>
2025-01-28 13:12:34 +0530

    collections updated

---
+++ src/com/demo/collection/EmployeeNameComparator.java
@@ -0,0 +1,14 @@
+package com.demo.collection;
+
+import java.util.Comparator;
+
+public class EmployeeNameComparator implements Comparator<Employee> {
+
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		// TODO Auto-generated method stub
+		return e1.getName().compareTo(e2.getName()); // our logic
+
+	}
+
+}
